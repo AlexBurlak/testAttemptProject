ac4a00e9e19ef0a8891cb2d93a0dc38bb475966e
bomjyk <bomjyk@gmail.com>
2021-07-23 15:19:56 +0300

    Added db context and connection string

--- TestAttempProject/TestAttempProject/Startup.cs
+++ TestAttempProject/TestAttempProject/Startup.cs
@@ -3,19 +3,33 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using TestAttemptProject.DAL.Context;
+using Microsoft.Extensions.Configuration;
+using Swashbuckle.AspNetCore;
 
 namespace TestAttemptProject
 {
     public class Startup
     {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+        public IConfiguration Configuration;
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<TestAttemptDbContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("LocalDb")));
+            services.AddControllers();
+
+            services.AddSwaggerGen();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -24,16 +38,21 @@ namespace TestAttemptProject
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c =>
+                {
+                    c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+                });
             }
 
+            app.UseHttpsRedirection();
             app.UseRouting();
+
+            app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapControllers();
             });
         }
     }
